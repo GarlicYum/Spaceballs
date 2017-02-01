6eacf7e485ab6361a520e22e76bad3c806727fbe
Yumtard <staffan.persson@hotmail.com>
2017-02-01 01:15:12 +0100

    stta

--- Engine/ShieldManager.cpp
+++ Engine/ShieldManager.cpp
@@ -1,6 +1,16 @@
 #include "ShieldManager.h"
 #include "Ship.h"
 
+ShieldManager::ShieldManager()
+{	
+	std::mt19937 rng;
+	std::uniform_real_distribution<float> xDist(0.0f, 700.0f);
+	for (int i = 0; i < nShieldMax; i++)
+	{
+		smallShield[i].SetPos(xDist(rng));
+	}	
+}
+
 void ShieldManager::Update(Ship & ship)
 {
 	shield.Update(ship);
--- Engine/ShieldManager.h
+++ Engine/ShieldManager.h
@@ -3,11 +3,13 @@
 #include "Ship.h"
 #include "Shield.h"
 #include "Graphics.h"
+#include <random>
 
 
 class ShieldManager
 {
 public:
+	ShieldManager();
 	void Update(Ship& ship);
 	void Draw(Graphics& gfx);
 	Shield& GetShield();
